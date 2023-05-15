5684745cc3ab59db4e30dd2227e5452e7080b9fc
BeloIV <118690695+BeloIV@users.noreply.github.com>
2023-05-15 20:49:49 +0200

    upravujeme posledne veci

--- aaaa.py
+++
@@ -1,1 +0,0 @@
-from testujee.message import EmailMessage
--- sss.py
+++
@@ -1,4 +0,0 @@
-from subprocess import call
-
-
-call(["python","{}".format("/Users/stefanbelusko/Desktop/skola/programovanie/Semestralny_2/aaaas.py")])
